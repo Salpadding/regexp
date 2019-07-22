2b311f16fc964073d857607a5cbcfef7b48278e9
Sal <m6567fc@outlook.com>
2019-07-22 23:53:58 +0800

    thompson construction

--- nfa.v2/nfa.go
+++ nfa.v2/nfa.go
@@ -2,6 +2,10 @@ package nfa_v2
 
 type state int
 
+const (
+	epsilon = 0xffffffff
+)
+
 func newStateSet() stateSet {
 	return make(map[state]bool)
 }
@@ -20,6 +24,21 @@ func (set stateSet) add(s state) stateSet {
 func (set stateSet) remove(s state) stateSet {
 	set[s] = false
 	return set
+}
+
+func (set stateSet) union(s2 stateSet) stateSet {
+	res := make(map[state]bool)
+	for s, ok := range set {
+		if ok {
+			res[s] = true
+		}
+	}
+	for s, ok := range s2 {
+		if ok {
+			res[s] = true
+		}
+	}
+	return res
 }
 
 func (set stateSet) copy() stateSet {
@@ -67,49 +86,31 @@ func (set stateSet) intersection(s2 stateSet) stateSet {
 	return res
 }
 
+// NFA is non deterministic automaton
 type NFA struct {
-	currentStates      stateSet
-	finalStates        stateSet
-	transitions        map[rune]map[state]state
-	epsilonTransitions map[state]state
-	rejected           bool
+	// in nfa, we assume the initial state is always zero
+	// all possibly state continuously starts from zero
+
+	finalStates stateSet                    // accepted states or final states
+	transitions map[rune]map[state]stateSet // transitions, may contains epsilon transitions
+
+	maximumState state // maximum state
 }
 
-func (n *NFA) Input(r rune) {
-	if n.rejected {
-		return
-	}
-	t, ok := n.transitions[r]
-	if !ok {
-		n.rejected = true
-		return
-	}
-	for _, s := range n.currentStates.elements() {
-		n.currentStates.remove(s)
-		s1, ok := t[s]
-		if !ok {
-			continue
-		}
-		n.currentStates.add(s1)
-	}
-	if n.currentStates.size() == 0 {
-		n.rejected = true
-		return
-	}
-}
-
-func (n *NFA) Accepted() bool {
-	if n.rejected {
-		return false
-	}
-	closure := n.currentStates.copy()
+// epsilon closure of states
+func (n *NFA) closure(set stateSet) stateSet {
+	closure := stateSet{}.copy()
 	fn := func() {
+		epsilonTransitions, ok := n.transitions[epsilon]
+		if !ok {
+			return
+		}
 		for _, s := range closure.elements() {
-			s1, ok := n.epsilonTransitions[s]
+			s1, ok := epsilonTransitions[s]
 			if !ok {
 				continue
 			}
-			closure.add(s1)
+			closure = closure.union(s1)
 		}
 	}
 	size0 := closure.size()
@@ -121,16 +122,135 @@ func (n *NFA) Accepted() bool {
 		}
 		size0 = size1
 	}
-	return closure.intersection(n.finalStates).size() > 0
+	return closure
 }
 
 func NewRune(r rune) *NFA {
 	return &NFA{
-		currentStates: newStateSet().add(0),
-		finalStates:   newStateSet().add(1),
-		transitions: map[rune]map[state]state{
-			r: {0: 1},
+		finalStates: newStateSet().add(1),
+		transitions: map[rune]map[state]stateSet{
+			r: {0: newStateSet().add(1)},
 		},
-		epsilonTransitions: make(map[state]state),
+		maximumState: 1,
 	}
+}
+
+// util method for thompson construction
+func (n *NFA) offset(i int) *NFA {
+	finalStates := make(map[state]bool, len(n.finalStates))
+	for k, ok := range n.finalStates {
+		if !ok {
+			continue
+		}
+		finalStates[k+state(i)] = true
+	}
+	transitions := make(map[rune]map[state]stateSet, len(n.transitions))
+	for r, v := range n.transitions {
+		transitions[r] = make(map[state]stateSet, len(v))
+		for l, m := range v {
+			m2 := newStateSet()
+			for _, s := range m.elements() {
+				m2.add(s + state(i))
+			}
+			transitions[r][l+state(i)] = m2
+		}
+	}
+
+	return &NFA{
+		transitions:  transitions,
+		finalStates:  finalStates,
+		maximumState: n.maximumState + state(i),
+	}
+}
+
+func unionTrans(t1 map[state]stateSet, t2 map[state]stateSet) map[state]stateSet {
+	res := make(map[state]stateSet)
+	for k, v := range t1 {
+		res[k] = v
+	}
+	for k, v := range t2 {
+		res[k] = v
+	}
+	return res
+}
+
+func unionTransitions(tss1 map[rune]map[state]stateSet, tss2 map[rune]map[state]stateSet) map[rune]map[state]stateSet {
+	res := make(map[rune]map[state]stateSet)
+	for r, ts := range tss1 {
+		_, ok := res[r]
+		if !ok {
+			res[r] = make(map[state]stateSet)
+		}
+		res[r] = unionTrans(ts, res[r])
+	}
+	for r, ts := range tss2 {
+		_, ok := res[r]
+		if !ok {
+			res[r] = make(map[state]stateSet)
+		}
+		res[r] = unionTrans(ts, res[r])
+	}
+	return res
+}
+
+func (n *NFA) addTransition(r rune, from, to state) {
+	_, ok := n.transitions[r]
+	if !ok {
+		n.transitions[r] = make(map[state]stateSet)
+	}
+	_, ok = n.transitions[r][from]
+	if !ok {
+		n.transitions[r][from] = newStateSet()
+	}
+	n.transitions[r][from].add(to)
+}
+
+// thompson construction
+func (n *NFA) concat(n1 *NFA) *NFA {
+	n2 := n1.offset(int(n.maximumState) + 1)
+	res := &NFA{
+		finalStates:  n2.finalStates,
+		transitions:  unionTransitions(n.transitions, n2.transitions),
+		maximumState: n1.maximumState,
+	}
+	for _, s := range n.finalStates.elements() {
+		res.addTransition(epsilon, s, n.maximumState+1)
+	}
+	return res
+}
+
+// thompson construction
+func (n *NFA) or(n1 *NFA) *NFA {
+	n3 := n.offset(1)
+	n4 := n1.offset(int(n3.maximumState) + 1)
+	res := &NFA{
+		finalStates:  newStateSet().add(n4.maximumState + 1),
+		transitions:  unionTransitions(n3.transitions, n4.transitions),
+		maximumState: n4.maximumState + 1,
+	}
+	res.addTransition(epsilon, 0, 1)
+	res.addTransition(epsilon, 0, n3.maximumState+1)
+	for _, s := range n3.finalStates.elements() {
+		res.addTransition(epsilon, s, n4.maximumState+1)
+	}
+	for _, s := range n4.finalStates.elements() {
+		res.addTransition(epsilon, s, n4.maximumState+1)
+	}
+	return res
+}
+
+func (n *NFA) kleen() *NFA {
+	n1 := n.offset(1)
+	res := &NFA{
+		finalStates:  newStateSet().add(n1.maximumState + 1),
+		transitions:  n1.transitions,
+		maximumState: n1.maximumState + 1,
+	}
+	res.addTransition(epsilon, 0, 1)
+	res.addTransition(epsilon, 0, n1.maximumState+1)
+	for _, s := range n1.finalStates.elements() {
+		res.addTransition(epsilon, s, 1)
+		res.addTransition(epsilon, s, n1.maximumState+1)
+	}
+	return res
 }
