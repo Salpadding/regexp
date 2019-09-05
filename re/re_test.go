38db3cf7f43ecc3a69944b3dd898d1cab19a9c58
zhuyingjie <m6567fc@outlook.com>
2019-09-05 12:40:48 +0800

    Update re_test.go

--- re/re_test.go
+++ re/re_test.go
@@ -2,6 +2,7 @@ package re
 
 import (
 	"github.com/stretchr/testify/assert"
+	regexp2 "regexp"
 	"testing"
 )
 
@@ -54,4 +55,28 @@ func TestWisdomURL(t *testing.T) {
 	assert.True(t, re.Match("wisdom://192.168.0.104:9005"))
 	assert.True(t, re.Match("wisdom://localhost"))
 	assert.False(t, re.Match("wisdom://76a3f5787062ffd12425b27e14f29348a7407b42ebfdff8e14543e6356e10530@192.168.0:9005"))
+}
+
+func Benchmark1(b *testing.B) {
+	re, err := Compile(`wisdom://([0-9a-f]+@)?((\d+\.\d+\.\d+\.\d+)|[0-9a-zA-Z]+)(:[0-9]+)?`)
+	if err != nil{
+		b.Fail()
+	}
+	b.StartTimer()
+	for i := 0; i < 100000; i++{
+		re.Match("wisdom://76a3f5787062ffd12425b27e14f29348a7407b42ebfdff8e14543e6356e10530@192.168.0.104:9005")
+	}
+	b.StopTimer()
+}
+
+func Benchmark2(b *testing.B) {
+	re, err := regexp2.Compile(`wisdom://([0-9a-f]+@)?((\d+\.\d+\.\d+\.\d+)|[0-9a-zA-Z]+)(:[0-9]+)?`)
+	if err != nil{
+		b.Fail()
+	}
+	b.StartTimer()
+	for i := 0; i < 100000; i++{
+		re.MatchString("wisdom://76a3f5787062ffd12425b27e14f29348a7407b42ebfdff8e14543e6356e10530@192.168.0.104:9005")
+	}
+	b.StopTimer()
 }
